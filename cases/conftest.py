69efda1dd0c351db7af2df6928f733e395cf9d8b
@xie-wendong <2500359606@qq.com>
2024-07-30 17:41:36 +0800

    修改了驱动

--- cases/conftest.py
+++ cases/conftest.py
@@ -1,10 +1,13 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from config.config import *
 
 @pytest.fixture(scope="session")
 def login():
-    driver=webdriver.Chrome()
+    driver_path = "cases/chromedriver.exe"
+    service = Service(driver_path)
+    driver = webdriver.Chrome(service=service)
     driver.maximize_window()
     driver.get(URL)
     yield driver
